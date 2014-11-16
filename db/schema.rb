09d01cdf3cbaf5c2db5610c8cff5f20f359c27a4
Daniel Kehoe <daniel@danielkehoe.com>
2014-11-16 04:39:42 -0800

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116123902) do
+ActiveRecord::Schema.define(version: 20141116123907) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141116123902) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    
-  end
+    name "Test User"
+    email "test@example.com"
+    password "please123"
 
+    trait :admin do
+      role 'admin'
+    end
+
+  end
 end
